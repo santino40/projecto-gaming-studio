025a7d6b4b2021754e49b5574cb5737ac0864725
santino40 <125075415+santino40@users.noreply.github.com>
2023-02-10 14:04:26 -0300

    un buen motor gaming

--- main.ts
+++ main.ts
@@ -1,4 +1,24 @@
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 C F A F G D C5 ", 92)
+    bitbot.buzz(true)
+    bitbot.setLedColor(0xFF0000)
+})
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    # . # . #
+    . . . . .
+    . . # . .
+    `)
 music.playMelody("F D C5 E A D F C ", 120)
 basic.forever(function () {
-	
+    bitbot.setTalon(40)
+    bitbot.BBBias(BBRobotDirection.Left, 39)
+    bitbot.ledShift()
+    bitbot.setPixel(16, 0xFF0000)
+    bitbot.rotatems(BBRobotDirection.Left, 60, 400)
+    bitbot.select_model(BBModel.Classic)
+    bitbot.bbEnableBluetooth(BBBluetooth.btEnable)
+    bitbot.rotate(BBRobotDirection.Left, 60)
+    basic.showArrow(ArrowNames.East)
 })
